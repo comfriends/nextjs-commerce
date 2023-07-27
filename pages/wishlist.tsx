7af8368875b7eab0707a807eeb6d7089538091f6
comfriends <ortrortr@naver.com>
2023-07-27 10:57:00 +0900

    feat: order

--- pages/wishlist.tsx
+++ pages/wishlist.tsx
@@ -20,35 +20,41 @@ export default function Wishlist() {
   return (
     <div>
       <p className="text-2xl mb-4">내가 찜한 상품</p>
-      {products && (
-        <div className="grid grid-cols-3 gap-5">
-          {products.map((item) => (
-            <div
-              key={item.id}
-              style={{ maxWidth: 310 }}
-              onClick={() => router.push(`/products/${item.id}`)}
-            >
-              <Image
-                className="rounded"
-                alt={item.name}
-                src={item.image_url ?? ''}
-                width={310}
-                height={390}
-                placeholder="blur"
-                blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mM0tbSsBwACegEoriWGfgAAAABJRU5ErkJggg=="
-              />
-              <div className="flex">
-                <span>{item.name}</span>
-                <span className="ml-auto">
-                  {item.price.toLocaleString('ko-KR')}원
+      {products ? (
+        products.length > 0 ? (
+          <div className="grid grid-cols-3 gap-5">
+            {products.map((item) => (
+              <div
+                key={item.id}
+                style={{ maxWidth: 310 }}
+                onClick={() => router.push(`/products/${item.id}`)}
+              >
+                <Image
+                  className="rounded"
+                  alt={item.name}
+                  src={item.image_url ?? ''}
+                  width={310}
+                  height={390}
+                  placeholder="blur"
+                  blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mM0tbSsBwACegEoriWGfgAAAABJRU5ErkJggg=="
+                />
+                <div className="flex">
+                  <span>{item.name}</span>
+                  <span className="ml-auto">
+                    {item.price.toLocaleString('ko-KR')}원
+                  </span>
+                </div>
+                <span className="text-zinc-400">
+                  {CATEGORY_MAP[item.category_id - 1]}
                 </span>
               </div>
-              <span className="text-zinc-400">
-                {CATEGORY_MAP[item.category_id - 1]}
-              </span>
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
+        ) : (
+          <div>찜하기에 아무것도 없습니다.</div>
+        )
+      ) : (
+        <div>불러오는 중...</div>
       )}
     </div>
   )
